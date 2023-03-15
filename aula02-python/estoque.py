5e9d5b109f48310eae6567d638f08bbd42479216
Pedro Antunes <phnantunes@gmail.com>
2023-03-15 17:26:31 -0300

    Advancing the python homework. Update is completed

--- aula02-python/estoque.py
+++ aula02-python/estoque.py
@@ -27,13 +27,43 @@ def incluir():
     
 
 def listar():
+  response = requests.get(api_url)
+  data = response.json()
+
   print(f"\nListagem de Produtos")
   print("-"*40)
-  response = requests.get(api_url)
-  print(response.json())
+  if len(data) == 0:
+    print("Nenhum produto em estoque")
+  else:
+    print("ID --------- Produto -------------- Marca -------------- Estoque -------------- Preço")
+    for key in data:
+      print(f'{str(key["id"]):13s}{key["description"]:22s} {key["brand"]:20s} {str(key["quantityStock"]):22s} R$ {key["price"]:<4.2f}')
 
 def alterar():
-  pass
+  response = requests.get(api_url)
+  data = response.json()
+
+  listar()
+  id = int(input(f'Escolha o ID do Produto que você deseja alterar o preço e a quantidade: '))
+  quantity = int(input(f'Qual é a nova quantidade do produto em estoque: '))
+  price = float(input(f'Qual é o novo valor do produto: '))
+  productUpdate = {}
+
+  for key in data:
+    if key["id"] == id:
+      productUpdate = {
+        "description": key["description"],
+        "brand": key["brand"],
+        "quantityStock": quantity,
+        "price": price
+      }
+  response = requests.put(f"{api_url}/{id}", json=productUpdate)
+  response.json()
+  if response.status_code == 200:
+    print("="*30)
+    print("Produto alterado com sucesso!")
+    print("="*30)
+
 
 def excluir():
   pass
