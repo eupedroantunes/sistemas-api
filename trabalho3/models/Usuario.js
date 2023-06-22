6f3100e2812587af49d7bd93beedac9b82b34dbc
phnantunes <phnantunes@gmail.com>
2023-06-22 10:12:05 -0300

    First model

---
+++ trabalho3/models/Usuario.js
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt'
+
+import { sequelize } from '../databases/connect.js';
+
+export const Usuario = sequelize.define('usuario', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nome: {
+    type: DataTypes.STRING(40),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  senha: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+  lastLogin: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  token: {
+    type: DataTypes.NUMBER(4),
+    allowNull: true
+  }
+});
+
+// Hook (gancho) do Sequelize que é executado antes 
+// da inserção de um registro.
+// Faz a criptografia da senha e atribui o hash ao campo senha
+Usuario.beforeCreate(usuario => {
+  const salt = bcrypt.genSaltSync(12)
+  const hash = bcrypt.hashSync(usuario.senha, salt)
+  usuario.senha = hash  
+});
