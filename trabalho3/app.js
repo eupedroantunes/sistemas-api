7fb7e0f253cbd8b905ef5bcf574846be9035b00a
phnantunes <phnantunes@gmail.com>
2023-06-26 10:24:35 -0300

    Fixing app and routes

--- trabalho3/app.js
+++ trabalho3/app.js
@@ -1,34 +1,36 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
 
-import { sequelize } from './databases/connect.js';
-import { Usuario } from './models/Usuario.js';
-import { Log } from './models/Log.js';
+import { sequelize } from "./databases/connect.js";
+import { Usuario } from "./models/Usuario.js";
+import { Log } from "./models/Log.js";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
 async function connect_db() {
   try {
     await sequelize.authenticate();
-    console.log('Conexão com o banco de dados realizada com sucesso')
+    console.log("Conexão com o banco de dados realizada com sucesso");
 
-    await Usuario.sync()
-    await Log.sync()
+    await Usuario.sync();
+    await Log.sync();
   } catch (error) {
-    console.error('Erro na conexão com o banco: ', error);
+    console.error("Erro na conexão com o banco: ", error);
   }
 }
 
 connect_db();
 
-app.get('/', (req, res) => {
-  res.send('API do Trabalho #03')
+app.get("/", (req, res) => {
+  res.send("API do Trabalho #03");
 });
 
 app.listen(port, () => {
-  console.log(`Servidor Rodando na Porta: ${port}`)
-})
+  console.log(`Servidor Rodando na Porta: ${port}`);
+});
--- trabalho3/routes.js
+++ trabalho3/routes.js
@@ -1,6 +1,12 @@
 import { Router } from "express"
-import { usuarioIndex } from "./controllers/usuarioController"
+import { usuarioCreate, usuarioIndex } from "./controllers/usuarioController.js"
+import { loginUsuario } from "./controllers/loginController.js"
+import { verificaLogin } from "./middlewares/verificaLogin.js"
 
 const router = Router()
 
-router.get('/users', usuarioIndex)
+router.get('/users', verificaLogin, usuarioIndex)
+  .post('/users', usuarioCreate)
+  .get('/login', loginUsuario)
+
+export default router
